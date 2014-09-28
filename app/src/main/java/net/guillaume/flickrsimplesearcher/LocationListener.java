e7fede164376d5610aa30c27e1617df79cddfd51
guillaume <guillaume.sec+github@gmail.com>
2014-09-28 02:56:53 +0200

    Handle unavailable location provider and clear location on provider unavailability
    Fixes #1

--- app/src/main/java/net/guillaume/flickrsimplesearcher/LocationListener.java
+++ app/src/main/java/net/guillaume/flickrsimplesearcher/LocationListener.java
@@ -4,6 +4,7 @@ import android.location.Location;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.util.Log;
 
 import com.google.common.base.Optional;
 import com.squareup.otto.Bus;
@@ -18,10 +19,17 @@ import javax.inject.Singleton;
 @Singleton
 public class LocationListener implements android.location.LocationListener {
 
+    private static final String LOG_TAG                    = LocationListener.class.getSimpleName();
+
+    /**
+     * The minimum distance change for us to be notified about location updates.
+     */
+    private static final float  MIN_DISTANCE_UPDATE_METERS = 200;
+
     private LocationData mLastLocation;
 
     private final LocationManager mLocationManager;
-    private final Bus mApplicationBus;
+    private final Bus             mApplicationBus;
 
     @Inject
     public LocationListener(final LocationManager locationManager, final @ForApplication Bus applicationBus) {
@@ -31,16 +39,26 @@ public class LocationListener implements android.location.LocationListener {
         mApplicationBus.register(this);
     }
 
-    public void startListening() {
-        mLocationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, this);
+    /**
+     * @return true if it actually started listening, false otherwise
+     */
+    public boolean startListening() {
+        try {
+            mLocationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, MIN_DISTANCE_UPDATE_METERS, this);
+            return true;
+        } catch (final IllegalArgumentException illegalArgumentException) {
+            Log.i(LOG_TAG, "Location provider unavailable", illegalArgumentException);
+            return false;
+        }
     }
 
     public void stopListening() {
         mLocationManager.removeUpdates(this);
+        clearLocationData();  // since from now on we will have no idea of the actual location
     }
 
     public void clearLocationData() {
-        mLastLocation = null;
+        updateLocation(null);
     }
 
     @Produce public LocationChangeEvent produceLastLocationData() {
@@ -48,8 +66,7 @@ public class LocationListener implements android.location.LocationListener {
     }
 
     @Override public void onLocationChanged(final Location location) {
-        mLastLocation = LocationData.create(location.getLatitude(), location.getLongitude());
-        mApplicationBus.post(produceLastLocationData());
+        updateLocation(LocationData.create(location.getLatitude(), location.getLongitude()));
     }
 
     @Override public void onStatusChanged(final String provider, final int status, final Bundle extras) {
@@ -57,11 +74,16 @@ public class LocationListener implements android.location.LocationListener {
     }
 
     @Override public void onProviderEnabled(final String provider) {
-        // no use for us now
+        // nothing to do here: just wait for a location to be available
     }
 
     @Override public void onProviderDisabled(final String provider) {
-        // no use for us now
+        clearLocationData();
+    }
+
+    private void updateLocation(final @Nullable LocationData locationData) {
+        mLastLocation = locationData;
+        mApplicationBus.post(produceLastLocationData());
     }
 
 
